12510949de861bec10c316479bd7eb44c8c2d93e
Alex Tsiakalou <altsiakalou@gmail.com>
2025-06-30 11:52:26 +0200

    change training batch size for kaggle workaround

--- examples/trl/sft_trainer_lora.py
+++ examples/trl/sft_trainer_lora.py
@@ -122,7 +122,7 @@ class PeftSftTrainer(BasePlayPen):
                 eval_strategy="steps",
                 max_steps=10,
                 logging_steps=1,
-                per_device_train_batch_size=2
+                per_device_train_batch_size=1
             )
             
             # Initialize trainer context
