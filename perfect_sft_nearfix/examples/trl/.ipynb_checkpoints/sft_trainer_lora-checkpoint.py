2d000c84b1460895b9232fa3b2d9f0e2b26b0e55
Iana Arefeva <163318410+IanaArefeva@users.noreply.github.com>
2025-09-23 09:23:46 +0300

    Update sft_trainer_lora-checkpoint.py

--- perfect_sft_nearfix/examples/trl/.ipynb_checkpoints/sft_trainer_lora-checkpoint.py
+++ perfect_sft_nearfix/examples/trl/.ipynb_checkpoints/sft_trainer_lora-checkpoint.py
@@ -12,31 +12,20 @@ class PeftSftTrainer(BasePlayPen):
 
     def __init__(self, learner: HuggingfaceLocalModel):
         super().__init__(learner)
-        # Note: We configure the proper chat template for the tokenizer already during model loading in the backend
 
     def learn(self, game_registry: GameRegistry):
-        # Load a conversational dataset for SFT, that is, a list of "messages" -- basically tuples of role and content.
-        # The role can be "user" or "assistant" and typically alternates within the list.
-        # During training, everything up to the last assistant message becomes the prefix for prediction.
-        # The loss is calculated based on the differences to the last assistant message.
-        # Here we load the canonical training split as available in the huggingface playpen-data repository.
-        # By default, the dataset is stored in ~/.cache/huggingface/datasets/ on your machine. This might take a while.
         dataset = load_dataset("colab-potsdam/playpen-data", "interactions", split="train")
 
-        # Only use the episodes we are interested to train on: here all episodes with successful outcome
         dataset = dataset.filter(lambda episode: episode["meta"]["outcome"] == "success")
 
-        # We shuffle and split the remaining filtered samples to receive a test split
         dataset = dataset.train_test_split(0.2, shuffle=True, seed=42)
 
-        # Initialize training configuration
-        config = trl.SFTConfig(  # inherits TrainingArguments
+        config = trl.SFTConfig(  
             max_length=300,
             output_dir=f"models/sft+lora/{self.learner.get_name()}",
             eval_strategy="epoch"
         )
 
-        # Initialize trainer context
         trainer = trl.SFTTrainer(
             model=self.learner.model,
             train_dataset=dataset["train"],
@@ -52,9 +41,5 @@ class PeftSftTrainer(BasePlayPen):
             )
         )
 
-        # Train on the dataset; this will save only the adapters to the checkpoints directory
         trainer.train()
 
-        # Optional: Uncomment these lines to merge and save directly
-        # merged_model = trainer.model.merge_and_unload()
-        # merged_model.save_pretrained(f"models/sft+lora/{self.learner.get_name()}")
